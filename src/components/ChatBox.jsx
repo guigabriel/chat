5e44941a9d3e08139bec591bf3554dc71948ba53
Guilherme <Guilhermelisboa98@outlook.com>
2022-03-15 21:04:29 -0300

    supabase

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -9,7 +9,7 @@ import { ButtonSendSticker } from './ButtonSendSticker';
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 function escutaMensagemEmTempoReal(attMensagem) {
     return supabaseClient
